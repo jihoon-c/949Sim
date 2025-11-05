5d2faca3452e08212a87367a35482e703819c050
GonoBae <qldrhqorhsh@naver.com>
2025-11-05 23:37:07 +0900

    Base Project

---
+++ Source/Sim949/Sim949GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Sim949GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIM949_API ASim949GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
