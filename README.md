e04ece6b2ee517852653813d7bf8b207ef71c136
TumHardy <91581227+jihoon-c@users.noreply.github.com>
2025-11-05 22:10:29 +0900

    251105)5.6프로젝트 폐기

--- README.md
+++
@@ -1,2 +0,0 @@
-# 949Sim
-Aircraft Simulator
--- Source/Sim949/Sim949.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Sim949.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sim949, "Sim949" );
